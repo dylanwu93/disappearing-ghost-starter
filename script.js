6b78f52ab45b64c174cb918baa536865c2c0a052
Glitch (dylanwu93-disappearing-ghost-starter) <none>
2020-12-17 00:36:39 +0000

    🍍📮 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+$(".hide-button").click(function() {
+  $("img").hide();
+})
 
+$('.show-button').click(function() {
+  $('img').show();
+})
+$('.update-img-button').click(function() {
+  $('img').attr("src", "https://i.pinimg.com/originals/86/2e/32/862e322efc34712d4bbe503707bf7994.jpg");
+})
+$(".message-button").click(function(){
+    let val = $(".input").val();
+	    $(".message").append(val);
+})
+
+$('.name-button').click(function() {
+   let val = $(".input").val();
+  $("h1").text(val);
+})
+$('.original').click(function() {
+  $('img').attr("src", "https://ih1.redbubble.net/image.153656513.6605/flat,800x800,075,f.jpg");
+  $('img').show();
+  
+})
+                     
